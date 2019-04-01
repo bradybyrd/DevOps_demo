f786ae0cdf67787add95150a93bc0aa921d69719
TeamWork <bradyb@dbmaestro.com>
2019-04-01 12:38:21 -0700

    Add db export files [Version: 0.0.6__UrbanPop]

--- oracle/MultiBranch/MULTI_TSK/TABLE/LOCATIONS.sql
+++ oracle/MultiBranch/MULTI_TSK/TABLE/LOCATIONS.sql
@@ -1,48 +1,49 @@
-CREATE TABLE "MULTI_TSK"."LOCATIONS" 
+CREATE TABLE "LOCATIONS" 
    (	"LOCATION_ID" NUMBER(4,0), 
 	"STREET_ADDRESS" VARCHAR2(40), 
 	"POSTAL_CODE" VARCHAR2(12), 
 	"CITY" VARCHAR2(30), 
 	"STATE_PROVINCE" VARCHAR2(25), 
-	"COUNTRY_ID" CHAR(2)
+	"COUNTRY_ID" CHAR(2), 
+	"ELEVATION" NUMBER, 
+	"RAINFALL" NUMBER
    ) ;
-
-   COMMENT ON COLUMN "MULTI_TSK"."LOCATIONS"."LOCATION_ID" IS 'Primary key of locations table';
  
-   COMMENT ON COLUMN "MULTI_TSK"."LOCATIONS"."STREET_ADDRESS" IS 'Street address of an office, warehouse, or production site of a company.
+   COMMENT ON COLUMN "LOCATIONS"."LOCATION_ID" IS 'Primary key of locations table';
+ 
+   COMMENT ON COLUMN "LOCATIONS"."STREET_ADDRESS" IS 'Street address of an office, warehouse, or production site of a company.
 Contains building number and street name';
  
-   COMMENT ON COLUMN "MULTI_TSK"."LOCATIONS"."POSTAL_CODE" IS 'Postal code of the location of an office, warehouse, or production site
+   COMMENT ON COLUMN "LOCATIONS"."POSTAL_CODE" IS 'Postal code of the location of an office, warehouse, or production site
 of a company. ';
  
-   COMMENT ON COLUMN "MULTI_TSK"."LOCATIONS"."CITY" IS 'A not null column that shows city where an office, warehouse, or
+   COMMENT ON COLUMN "LOCATIONS"."CITY" IS 'A not null column that shows city where an office, warehouse, or
 production site of a company is located. ';
  
-   COMMENT ON COLUMN "MULTI_TSK"."LOCATIONS"."STATE_PROVINCE" IS 'State or Province where an office, warehouse, or production site of a
+   COMMENT ON COLUMN "LOCATIONS"."STATE_PROVINCE" IS 'State or Province where an office, warehouse, or production site of a
 company is located.';
  
-   COMMENT ON COLUMN "MULTI_TSK"."LOCATIONS"."COUNTRY_ID" IS 'Country where an office, warehouse, or production site of a company is
+   COMMENT ON COLUMN "LOCATIONS"."COUNTRY_ID" IS 'Country where an office, warehouse, or production site of a company is
 located. Foreign key to country_id column of the countries table.';
  
-   COMMENT ON TABLE "MULTI_TSK"."LOCATIONS"  IS 'Locations table that contains specific address of a specific office,
+   COMMENT ON TABLE "LOCATIONS"  IS 'Locations table that contains specific address of a specific office,
 warehouse, and/or production site of a company. Does not store addresses /
 locations of customers. Contains 23 rows; references with the
 departments and countries tables. ';
- 
 
-  CREATE INDEX "MULTI_TSK"."LOC_CITY_IX" ON "MULTI_TSK"."LOCATIONS" ("CITY") 
+  CREATE INDEX "LOC_CITY_IX" ON "LOCATIONS" ("CITY") 
   ;
  
-  CREATE INDEX "MULTI_TSK"."LOC_COUNTRY_IX" ON "MULTI_TSK"."LOCATIONS" ("COUNTRY_ID") 
+  CREATE INDEX "LOC_COUNTRY_IX" ON "LOCATIONS" ("COUNTRY_ID") 
   ;
  
-  CREATE INDEX "MULTI_TSK"."LOC_STATE_PROVINCE_IX" ON "MULTI_TSK"."LOCATIONS" ("STATE_PROVINCE") 
+  CREATE INDEX "LOC_STATE_PROVINCE_IX" ON "LOCATIONS" ("STATE_PROVINCE") 
   ;
-  
-  ALTER TABLE "MULTI_TSK"."LOCATIONS" ADD CONSTRAINT "LOC_C_ID_FK" FOREIGN KEY ("COUNTRY_ID")
-	  REFERENCES "MULTI_TSK"."COUNTRIES" ("COUNTRY_ID") ENABLE;
-  
-  ALTER TABLE "MULTI_TSK"."LOCATIONS" MODIFY ("CITY" CONSTRAINT "LOC_CITY_NN" NOT NULL ENABLE);
  
-  ALTER TABLE "MULTI_TSK"."LOCATIONS" ADD CONSTRAINT "LOC_ID_PK" PRIMARY KEY ("LOCATION_ID") ENABLE;
-  
+  ALTER TABLE "LOCATIONS" ADD CONSTRAINT "LOC_C_ID_FK" FOREIGN KEY ("COUNTRY_ID")
+	  REFERENCES "COUNTRIES" ("COUNTRY_ID") ENABLE;
+ 
+  ALTER TABLE "LOCATIONS" MODIFY ("CITY" CONSTRAINT "LOC_CITY_NN" NOT NULL ENABLE);
+ 
+  ALTER TABLE "LOCATIONS" ADD CONSTRAINT "LOC_ID_PK" PRIMARY KEY ("LOCATION_ID") ENABLE
+/
