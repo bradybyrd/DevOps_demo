97bd8d03472fd2a2787d0bb092a4331faadafdcf
TeamWork <bradyb@dbmaestro.com>
2019-04-29 10:20:48 -0700

    Update receiver for [Version: 4.3.34]

--- devops/build_receiver.bat
+++ devops/build_receiver.bat
@@ -2,7 +2,10 @@ echo off
 echo #--------------------------------------------#
 echo #      DBmaestro Build Processor         #
 echo #--------------------------------------------#
-java -cp ".;C:\Automation\dbm_demo\devops\lib\*;C:\Automation\dbm_demo\devops\lib\groovy-all-2.4.7.jar" groovy.ui.GroovyMain c:\Automation\local\build_receiver.groovy %*
+set
+set PWD=%~dp0
+REM echo PWD is %PWD%
+java -cp ".;%PWD%lib\*;%PWD%lib\groovy-all-2.4.7.jar" groovy.ui.GroovyMain "%PWD%build_receiver.groovy" %*
 IF %ERRORLEVEL% NEQ 0 ( 
    echo Groovy Script failed code: %ERRORLEVEL%
    EXIT /b %ERRORLEVEL% 
--- devops/build_receiver.groovy
+++ devops/build_receiver.groovy
@@ -1,4 +1,8 @@
-sep = "\\"
+sep = "\\" //FIXME Reset for unix
+base_path = new File(getClass().protectionDomain.codeSource.location.path).parent
+// Local Variables
+def pipeline = "HR_Tasks"
+def staging_path = "C:\\Automation\\MP\\${pipeline}"
 
 def shell_execute(cmd, path = "none"){
   def pth = ""
@@ -19,7 +23,7 @@ def shell_execute(cmd, path = "none"){
 
 
 def display_result(command, result){
-	separator()
+	println "#------------------------------------------------------#"
 	println "Running: ${command}"
 	println "out> " + result["stdout"]
 	println "err> " + result["stderr"]
@@ -28,4 +32,14 @@ def display_result(command, result){
 println "#=> Running Build Hook"
 def cmd = "cd C:\\Automation\\MultiBranch && @git log -1 HEAD --pretty=format:%s"
 def res = shell_execute(cmd)
+display_result(cmd,res)
+
+def dir_list = new File("${staging_path}${sep}REPORTS").listFiles()?.sort { -it.lastModified() }
+println "Reports: ${dir_list}"
+def picked = dir_list.head()
+println "Dir: ${picked}"
+def cur_report = new File(picked.toString()).listFiles()?.head()
+println "Picked: ${cur_report}"
+cmd = "cd C:\\Automation\\MultiBranch && @git log -1 HEAD --pretty=format:%s"
+res = shell_execute(cmd)
 display_result(cmd,res)
