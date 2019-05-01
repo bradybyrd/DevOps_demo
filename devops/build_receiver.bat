6ddd9937dea7a5097b276cf327b1c210267b255c
TeamWork <bradyb@dbmaestro.com>
2019-04-30 18:17:08 -0700

    pretty output

--- devops/build_receiver.bat
+++ devops/build_receiver.bat
@@ -2,7 +2,6 @@ echo off
 echo #--------------------------------------------#
 echo #      DBmaestro Build Processor         #
 echo #--------------------------------------------#
-set
 set PWD=%~dp0
 REM echo PWD is %PWD%
 java -cp ".;%PWD%lib\*;%PWD%lib\groovy-all-2.4.7.jar" groovy.ui.GroovyMain "%PWD%build_receiver.groovy" %*
