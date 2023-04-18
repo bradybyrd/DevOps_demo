7d29f29e1aa169d5b047c58622485f0b467e5eb6
Brady Byrd <brady.byrd@mongodb.com>
2023-04-18 07:53:19 -0400

    jenkinsfile edits

--- Jenkinsfile_test.groovy
+++ Jenkinsfile_test.groovy
@@ -1,26 +1,100 @@
-/*
-#---------- Jenkinsfile Stub ------------------#
-This file will call the central PipelineInclude module which contains the pipeline logic.  In this file you can
-put things specific to the branch that drives this work. Use the settings map to pass any variables into the
-master include.
-*/
-// Set this library variable in Jenkins settings Global Pipeline Libraries section
-@Library('DBmGlobal')_
+import groovy.json.*
 
-println "#----------- Remote Pipeline Execution ------------#"
-// The keys and values in settings will be added to (or overwrite) the settings in the local_settings.json file
-def settings = [
-	"pipeline" : "MultiBranch",
-  "settings_file" : "C:\\Automation\\jenkins_pipe\\local_settings_include.json"  
-]
-// Add a properties for Platform and Skip_Packaging more
+// proj Deployment Pipeline
+sep = "/"
+def base_path = ""
+
+rootJobName = "$env.JOB_NAME";
+template = "$env.Template";
+branchName = "master"
+branchVersion = ""
+def base_env = "Dev"
+def base_schema = ""
+def version = "3.11.2.1"
+def buildNumber = "$env.BUILD_NUMBER"
+
+// Add a properties for Platform and Skip_Packaging
 properties([
-        parameters([
-          string(name: 'Version', description: "Enter Version for the Deploy", defaultValue: "V0.0.0"),
-          choice(name: 'Skip_Packaging', description: "Yes/No to skip packaging step", choices: 'No\nYes')
-        ])
+	parameters([
+		string(name: 'Template', description: "template file in the Templates directory", default: 'm30_standard')
+	])
 ])
+/*
+parameters{ 
+  string(name: 'Version', description: "Enter Version for the Deploy", defaultValue: "V0.0.0"),
+  text(name: 'mytextparam', 
+                 defaultValue: 'Default lines for the parameter', 
+                 description: 'A description of this param')    
+}
+*/
 
-def pipe = new org.dbmaestro.PipelineInclude()
-pipe.execute(settings)
-println "Finished"
+/*
+#-----------------------------------------------#
+#  Stages
+*/
+
+stage("Environment Check") {
+  //node (dbmNode) {
+    //Copy from source to version folder
+  sh("env")
+}
+
+stage("Deploy") {
+	//input message: "Deploy to ${environment}?", submitter: approver
+	//node (dbmNode) {
+		//  Use shell script to call python
+		echo '#------------------- Performing Deploy on ${template} --------------#'
+		sh """
+      echo Running python command
+      python3 atlas_rest.py action=create_cluster template=${template}"""
+	//}   
+} 
+
+@NonCPS
+def ensure_dir(pth){
+  folder = new File(pth)
+  if ( !folder.exists() ){
+    if( folder.mkdirs()){
+        return true
+    }else{
+        return false
+    }
+  }else{
+    return true
+  }
+}
+
+def get_settings(file_path) {
+	def jsonSlurper = new JsonSlurper()
+	def settings = [:]
+	println "JSON Settings Document: ${file_path}"
+	def json_file_obj = new File( file_path )
+	if (json_file_obj.exists() ) {
+	  settings = jsonSlurper.parseText(json_file_obj.text)  
+	}
+	return settings
+}
+
+def message_box(msg, def mtype = "sep") {
+  def tot = 80
+  def start = ""
+  def res = ""
+  msg = (msg.size() > 65) ? msg[0..64] : msg
+  def ilen = tot - msg.size()
+  if (mtype == "sep"){
+    start = "#${"-" * (ilen/2).toInteger()} ${msg} "
+    res = "${start}${"-" * (tot - start.size() + 1)}#"
+  }else{
+    res = "#${"-" * tot}#\n"
+    start = "#${" " * (ilen/2).toInteger()} ${msg} "
+    res += "${start}${" " * (tot - start.size() + 1)}#\n"
+    res += "#${"-" * tot}#\n"   
+  }
+  //println res
+  return res
+}
+
+def separator( def ilength = 82){
+  def dashy = "-" * (ilength - 2)
+  //println "#${dashy}#"
+}
